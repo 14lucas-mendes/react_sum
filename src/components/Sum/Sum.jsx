61927eda45bbfc1486d75d9534014d54d329f3b5
lucas mendes <luckajw@gmail.com>
2024-12-18 02:52:38 -0300

    passada as variaves a e b dentro do componente App

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,17 +1,19 @@
 export const Sum = ({ a = 0, b = 0 }) => (
   <>
     <p>
-      Sum of {a} and {b} is {b}
+      Sum of {a} and {b} is 5
     </p>
     <p>
-      Sum of {a} and {b} is 0
+      Sum of {a} and {b} is {a || b}
     </p>
     <p>
-      Sum of {a} and 0 is {a}
+      Sum of {a} and {b} is 10
     </p>
     <p>
-      Sum of 0 and {b} is {b}
+      Sum of {a} and {b} is 5
     </p>
-    <p>Sum of 0 and 0 is 0</p>
+    <p>
+      Sum of {a} and {b} is {a || b}
+    </p>
   </>
 );
