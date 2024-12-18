944630c0de0b842dd2f00b5104ab310d72c4aa03
lucas mendes <luckajw@gmail.com>
2024-12-18 01:54:09 -0300

    resolution react_sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,17 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => (
+  <>
+    <p>
+      Sum of {a} and {b} is {b}
+    </p>
+    <p>
+      Sum of {a} and {b} is 0
+    </p>
+    <p>
+      Sum of {a} and 0 is {a}
+    </p>
+    <p>
+      Sum of 0 and {b} is {b}
+    </p>
+    <p>Sum of 0 and 0 is 0</p>
+  </>
+);
